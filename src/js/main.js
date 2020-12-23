c6c7370a4f97b43859089cfde7e0e50ac6959022
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2020-12-23 06:31:12 +0300

    fix bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
 import './vendor/focus-visible.min.js';
 import './_vars';
+import './functions/jquery.inputmask.min'
 import {resizeContent} from './functions/resize';
 import {scrollTo} from './functions/smooth-scroll';
 import { disableScroll, enableScroll } from './functions/stop-scroll';
 import './components/slider'
+import $ from "jquery";
 
 //disableScroll(fix) // fix -> class of element with position: fixed
+$(function () {
+  $("#phone").inputmask("+7 (999) 999-99-99" );
+
+});
